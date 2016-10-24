d918652e51dab8499e2f8b6bc3d4ced00864ec60
Ayodeji Oyewole <ayodeji.oyewole@gmail.com>
2016-10-24 00:49:07 -0400

    randomMST space and time optimization

--- RandomMST.java
+++ RandomMST.java
@@ -2,81 +2,59 @@ import java.util.ArrayList;
 
 public class RandomMST {
 	
-	public int noOfVertices;
-	public ArrayList <ArrayList<Float>> adjMatrix = new ArrayList<ArrayList<Float>>();
-	public ArrayList <ArrayList<Float>> F = new ArrayList<ArrayList<Float>>();
-	public ArrayList<Float> C = new ArrayList<Float>();
-	public ArrayList<Integer> E = new ArrayList<Integer>();
-	public ArrayList<Integer> Q = new ArrayList<Integer>();
+	public float mstSize = 0;
+	public float[] C;
+	public int[] E;
+	public ArrayList<Integer> Q;
 	
 	public RandomMST(int noOfVertices) {
-		this.noOfVertices = noOfVertices;
+		this.C = new float[noOfVertices];
+		this.E = new int[noOfVertices];
+		this.Q = new ArrayList<Integer>();
+
 		for (int i = 0; i < noOfVertices; i++) {
-			//New row in the adjacency matrix
-			ArrayList <Float> row = new ArrayList<Float>();
-			ArrayList <Float> rowF = new ArrayList<Float>();
-			//Fill up edges in row
-			for (int j = 0; j < noOfVertices; j++) {
-				rowF.add((float) -1);
-				if (i == j){
-					//Edges along the diagonal
-					row.add((float) -1);
-				} else if (j < i) {
-					//Matrix is diagonally symetric
-					row.add(this.adjMatrix.get(j).get(i));
-				} else {
-					row.add((float) Math.random());
-				}
-			}
-			
-			// Add row to adjMatrix
-			this.adjMatrix.add(row);    //0.87672, 0.3486, 0.76423, 0.14651, .....
-			this.F.add(rowF);
-			this.C.add(Float.MAX_VALUE); //%, %, %, %, .....
-			this.E.add(-1);      //-1, -1, -1, -1, -1, .....
-			this.Q.add(i+1);       //1, 2, 3, 4, ....... 100
+			this.C[i] = (float)2;
+			this.E[i] = -1;
+			this.Q.add(i + 1);
 		}
-		
 	}
 	
 	public void primsAlgorithm() {
 		while (!this.Q.isEmpty()) {
-			Float minQ = Float.MAX_VALUE;
+			float minQ = 2;
 			int minQIndex = 0;
 			// Find and remove a vertex v from Q having the minimum possible value of C[v]
 			for (int i = 0; i < this.Q.size(); i++) {
-				if (this.C.get(this.Q.get(i) - 1) < minQ){
-					minQ = this.C.get(this.Q.get(i) - 1);
+				if (this.C[this.Q.get(i) - 1] < minQ){
+					minQ = this.C[this.Q.get(i) - 1];
 					minQIndex = i;
 				}
 			}
 
 			int vertexV = this.Q.remove(minQIndex);
 			// Add v to F and, if E[v] is not the special flag value, also add E[v] to F
-			if (this.E.get(vertexV - 1) != -1) {
-				Float edgeWeight = this.adjMatrix.get(vertexV - 1).get(this.E.get(vertexV - 1) - 1);
-				this.F.get(vertexV - 1).set(this.E.get(vertexV - 1) - 1, edgeWeight);
+			if (this.E[vertexV - 1] != -1) {
+				this.mstSize += this.C[vertexV - 1];
 			}
 			
 			/* Loop over the edges vw connecting v to other vertices w. For each such edge, if w still
 			 * belongs to Q and vw has smaller weight than C[w], perform the following steps:
 																		Set C[w] to the cost of edge vw
 																		Set E[w] to point to edge vw.*/
-			ArrayList<Float> edgesVW = this.adjMatrix.get(vertexV - 1);
-			for (int w = 0; w < edgesVW.size(); w++) {
-				if ((edgesVW.get(w) != -1) && (this.Q.contains(w+1)) && (edgesVW.get(w) < this.C.get(w))) {
-					this.C.set(w, edgesVW.get(w));
-					this.E.set(w, vertexV);
-				}
+			for (int w = 0; w < this.Q.size(); w++) {
+				int column = this.Q.get(w) - 1;
+				float edgeVW = (float) Math.random();
+				if (edgeVW < C[column]) {
+					this.C[column] = edgeVW;
+					this.E[column] = vertexV;
+				}				
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-		int noOfVertices = Integer.parseInt(args[0]);
-		RandomMST graph = new RandomMST(noOfVertices);
-		System.out.println(graph.adjMatrix);
+		RandomMST graph = new RandomMST(Integer.parseInt(args[0]));
 		graph.primsAlgorithm();
-		System.out.println(graph.F);
+		System.out.println(graph.mstSize);
 	}
 }
